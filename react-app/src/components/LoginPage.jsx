77def8b89a094d9e00ada72aae71f445757229fc
Maenhabes <Maenhabes@yahoo.com>
2021-04-01 15:36:48 -0700

    finish styling home page for now

--- react-app/src/components/LoginPage.jsx
+++ react-app/src/components/LoginPage.jsx
@@ -5,10 +5,25 @@ import './LoginPage.css'
 export default function LoginPage() {
 
     return (
-        <div className="img">
+        <div className="outer-outer">
+            <div className="outer-text">
+                <div>
 
+            <h1 className="h12">Unlimited movies, TV shows, and more.</h1>
+                </div>
+                <div>
 
+            <h1 className="h13">
+            Ready for a tour? Sign up or click on login for a demo.
+            </h1>
+                </div>
+            </div>
 
+            <div  className="img">
+            </div>
+
+            {/* <h1 className="h12">Unlimited movies, TV shows, and more.</h1>
+            <h1 className="h12">Unlimited movies, TV shows, and more.</h1> */}
 
         </div>
     )
