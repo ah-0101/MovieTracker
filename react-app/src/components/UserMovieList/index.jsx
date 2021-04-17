37ce2eb54f7949384423aaed71536f309573bf49
Maenhabes <Maenhabes@yahoo.com>
2021-04-17 05:20:31 -0700

    bugy way for color index star

--- react-app/src/components/UserMovieList/index.jsx
+++ react-app/src/components/UserMovieList/index.jsx
@@ -103,15 +103,15 @@ export default  function UserMovieList(){
            </div>
            <div className="rate-container">
           <div className="rate">
-    <input type="radio" id="star5" name="rate" value="5" />
-    <label for="star5" title="text">5 stars</label>
-    <input type="radio" id="star4" name="rate" value="4" />
+    <input  id="star5" name="rate" value="5" />
+    <label style={{color:"skyBlue"}} for="star5" title="text">5 stars</label>
+    <input  id="star4" name="rate" value="4" />
     <label for="star4" title="text">4 stars</label>
-    <input type="radio" id="star3" name="rate" value="3" />
+    <input  id="star3" name="rate" value="3" />
     <label for="star3" title="text">3 stars</label>
-    <input type="radio" id="star2" name="rate" value="2" />
+    <input  id="star2" name="rate" value="2" />
     <label for="star2" title="text">2 stars</label>
-    <input type="radio" id="star1" name="rate" value="1" />
+    <input  id="star1" name="rate" value="1" />
     <label for="star1" title="text">1 star</label>
   </div>
           </div>
